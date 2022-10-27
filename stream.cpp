960e1cc1fcfaa83a40a9264f5a9e857f5d559a2b
Gautham <58910261+Gautham-N@users.noreply.github.com>
2022-10-27 13:37:46 +0530

    Update stream.cpp

--- stream.cpp
+++ stream.cpp
@@ -1,6 +1,8 @@
 // stream.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#include "test/catch.hpp"
 #include <iostream>
 #include <sstream>
 #include <cassert>
